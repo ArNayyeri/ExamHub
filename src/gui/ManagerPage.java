4064dbb755304bfe30f2fb268552472475c08f1b
trynade <roozbehsa3@gmail.com>
2020-07-15 22:51:35 +0430

    ui touches

--- src/gui/ManagerPage.java
+++ src/gui/ManagerPage.java
@@ -4,11 +4,11 @@
 
 package gui;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ManagerPage {
 
